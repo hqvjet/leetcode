c7d504dd70b15e1eca149a3fc1d0c4edf2a65bb4
Raphael Hoang <105470839+hqvjet@users.noreply.github.com>
2024-10-28 20:15:04 +0700

    Update 746.py

--- 746.py
+++ 746.py
@@ -1,8 +1,12 @@
 class Solution:
+
+    # idea: We can step 1 or 2 steps, then we will compare which step is more optimal,
+    # we save the previous optimal step, then compare (previous_step + cost of 1 step) and (previous_step + cost of 2 steps)
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         dp = [0, 0]
-        
+
+        # compare each step using dp formular below and update 'dp' array
         for i in range(2, len(cost)):
             dp.append(min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2])) 
-        
+
         return min(dp[-1] + cost[-1], dp[-2] + cost[-2])

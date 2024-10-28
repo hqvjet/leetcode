59a83c616bc1f5c867be3c6218b8242a4e6b9da2
Raphael Hoang <105470839+hqvjet@users.noreply.github.com>
2024-10-28 20:04:53 +0700

    Update 575.py

--- 575.py
+++ 575.py
@@ -1,4 +1,6 @@
 class Solution:
+
+    # idea: We will just simply pay attention on num of type and num of candies / 2 
     def distributeCandies(self, candyType: List[int]) -> int:
         n = len(candyType) // 2
         a = set(candyType)

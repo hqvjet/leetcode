49b30212a55f5e8550342fcf76c4c6b9fa8c8adc
Raphael Hoang <105470839+hqvjet@users.noreply.github.com>
2024-10-28 19:46:01 +0700

    Update 557.py
    update comments on 557

--- 557.py
+++ 557.py
@@ -1,9 +1,15 @@
 class Solution:
+
+    # idea: for each word of sentence, reverse these words and printout
     def reverseWords(self, s: str) -> str:
         res = ""
+
+        # split sentence by " "
         for i in s.split():
+            # reverse word and save to 'res' array
             res += i[::-1] + ' '
 
+        # remove the last ' '
         res = res[:-1]
 
         return res

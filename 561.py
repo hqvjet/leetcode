5335477130d55385c143bc6a734797610d5e37b7
Raphael Hoang <105470839+hqvjet@users.noreply.github.com>
2024-10-28 19:52:08 +0700

    Update 561.py
    add comments on 561

--- 561.py
+++ 561.py
@@ -1,8 +1,11 @@
 class Solution:
+
+    # idea: we have to pair the two closest numbers to get the optimal answer
     def arrayPairSum(self, nums: List[int]) -> int:
         nums.sort()
-
         s = 0
+        
+        # sum all of the first pair, jump 2 steps to skip the max(pair) 
         for i in range(0, len(nums), 2):
             s += nums[i]
 
